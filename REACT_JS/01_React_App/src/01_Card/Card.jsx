933a0bab86d4247790bb839093ce50e8b20c7fef
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-07 00:36:51 +0530

    Fixed isuue

--- REACT_JS/01_React_App/src/01_Card/Card.jsx
+++ REACT_JS/01_React_App/src/01_Card/Card.jsx
@@ -1,10 +1,13 @@
 function Card(props) {
+
+    console.log(props.user.name);
+
     return (
         <div className=" m-10 border w-fit bg-emerald-200 rounded-2xl p-5 flex flex-col justify-center items-center">
-            <img src={props.src} alt="userPhoto" className="w-20 h-20 rounded-full object-cover" />
-            <h1 className="text-2xl text-black font-bold">{props.name}</h1>
-            <h2 className="text-lg text-blue-700">{props.role}</h2>
-            <p className="text-gray-700">{props.location}</p>
+            <img src={props.user.src} alt="userPhoto" className="w-20 h-20 rounded-full object-cover" />
+            <h1 className="text-2xl text-black font-bold">{props.user.name}</h1>
+            <h2 className="text-lg text-blue-700">{props.user.role}</h2>
+            <p className="text-gray-700">{props.user.location}</p>
         </div>
     );
 }
--- REACT_JS/01_React_App/src/01_Card/main_01.jsx
+++ REACT_JS/01_React_App/src/01_Card/main_01.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 
 import Card from "./Card.jsx";
 
+const user = {
+    src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300",
+    name: "John Doe",
+    role: "Frontend Developer",
+    location: "New York"
+};
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Card
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300"
-            name="John Doe"
-            role="Frontend Developer"
-            location="New York"
-        />
+        <Card user={user} />
     </StrictMode>
 );
