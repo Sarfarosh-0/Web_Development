5bf4e2c24787b5d594fa516bb4a8d0219629e727
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-08 02:18:27 +0530

    Final

--- REACT_JS/01_React_App/src/04_Product_Inventory/Components/ProductGrid.jsx
+++ REACT_JS/01_React_App/src/04_Product_Inventory/Components/ProductGrid.jsx
@@ -3,18 +3,15 @@ import products from "../data/products";
 
 function ProductGrid() {
 
-    products.map((product) => (
-        <ProductCard />
-    ))
     return (
         <main className="grid grid-cols-4 grid-rows-2 gap-3 p-10">
             {products.map((product) => (
                 <ProductCard
-                 key={product.id}
-                 name={product.name}
-                 price={product.price}
-                 inStock={product.inStock}
-                  />
+                    key={product.id}
+                    name={product.name}
+                    price={product.price}
+                    inStock={product.inStock}
+                />
             ))}
         </main>
     )
