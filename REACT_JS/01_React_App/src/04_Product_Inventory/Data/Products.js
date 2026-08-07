d098f64114ed0616640fbf000a27b00ae797bc35
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-08 01:56:38 +0530

    Added Data

---
+++ REACT_JS/01_React_App/src/04_Product_Inventory/Data/Products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 59.99,
+    inStock: true,
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 129.99,
+    inStock: true,
+  },
+  {
+    id: 3,
+    name: "Phone Case",
+    price: 19.99,
+    inStock: false,
+  },
+  {
+    id: 4,
+    name: "Bluetooth Speaker",
+    price: 79.99,
+    inStock: true,
+  },
+  {
+    id: 5,
+    name: "Gaming Mouse",
+    price: 39.99,
+    inStock: false,
+  },
+  {
+    id: 6,
+    name: "Mechanical Keyboard",
+    price: 89.99,
+    inStock: true,
+  },
+  {
+    id: 7,
+    name: "Mechanical Keyboard",
+    price: 120.99,
+    inStock: true,
+  },
+  {
+    id: 8,
+    name: "Keyboard",
+    price: 19.99,
+    inStock: true,
+  },
+];
+
+export default products;
