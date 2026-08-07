55d3d2e3df8935e01828f874e6043147169c97ae
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-08 01:22:37 +0530

    Footer

--- REACT_JS/01_React_App/src/04_Product_Inventory/App.jsx
+++ REACT_JS/01_React_App/src/04_Product_Inventory/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     return (
         <div>
             <Header totalProduct={8} />
+            <Footer />
         </div>
     );
 }
