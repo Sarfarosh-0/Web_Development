8ff412f7c9ed144d5814665cbbbaebb9983a71b4
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-08 01:41:51 +0530

    Product Card

--- REACT_JS/01_React_App/src/04_Product_Inventory/App.jsx
+++ REACT_JS/01_React_App/src/04_Product_Inventory/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import ProductCard from "./Components/ProductCard";
 
 function App() {
 
     return (
         <div>
             <Header totalProduct={8} />
+            <ProductCard />
             <Footer />
         </div>
     );
---
+++ REACT_JS/01_React_App/src/04_Product_Inventory/Components/ProductCard.jsx
@@ -0,0 +1,18 @@
+function ProductCard() {
+    return (
+        <div className="bg-white m-2 p-3 w-fit rounded-lg shadow-md border border-slate-200 hover:scale-105 transition-all duration-500 ease-in-out">
+            <img src="https://placehold.co/250x150" alt="Product" className="object-cover rounded-lg" />
+
+            <div>
+                <h2 className="text-xl font-bold text-slate-800 mt-2"> Wireless Headphones </h2>
+                <p className="text-blue-600 text-lg font-semibold mt-1"> $59.99 </p>
+
+                <span className="inline-block mt-4 bg-green-100 text-green-700 px-4 py-2 rounded-full text-sm font-semibold">
+                    🟢 In Stock
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
