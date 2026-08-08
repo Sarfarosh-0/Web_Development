850261980389be6bb98ce25162c928e148d79f17
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-09 01:56:54 +0530

    Chaged Color theme

--- REACT_JS/01_React_App/src/05_User_Role_Filter/Components/Header.jsx
+++ REACT_JS/01_React_App/src/05_User_Role_Filter/Components/Header.jsx
@@ -1,21 +1,21 @@
 function Header({ totalProduct }) {
     return (
-        <header className="flex justify-between items-center border-b bg-white border-slate-300 w-full px-4 py-2 shadow-xl sticky top-0 z-10">
+        <header className="flex justify-between items-center border-b bg-slate-900 border-slate-800 text-slate-100 w-full px-4 py-2 shadow-xl sticky top-0 z-10">
             <div className="flex gap-4 items-center">
                 <span className="text-4xl">🫂</span>
                 <div>
-                    <h1 className="text-2xl font-bold">Team Roster</h1>
-                    <p className="text-slate-500 ">
+                    <h1 className="text-2xl font-bold text-white">Team Roster</h1>
+                    <p className="text-slate-400">
                         Browse and filter team members by role.
                     </p>
                 </div>
             </div>
 
-            <div className="bg-indigo-200 rounded-xl px-4 py-1 flex items-center gap-4">
+            <div className="bg-slate-800 border border-slate-700 rounded-xl px-4 py-1 flex items-center gap-4">
                 <span className="text-5xl">👥</span>
                 <div>
-                    <h2 className="text-3xl font-bold text-center text-blue-600">{totalProduct}</h2>
-                    <p className="text-slate-500 text-sm font-semibold">Total Members</p>
+                    <h2 className="text-3xl font-bold text-indigo-400">{totalProduct}</h2>
+                    <p className="text-slate-400 text-sm font-semibold">Total Members</p>
                 </div>
             </div>
         </header>
