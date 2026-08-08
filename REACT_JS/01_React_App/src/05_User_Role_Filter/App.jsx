06ec3233e5e7f12f081ef24c5693b44b3f00eb26
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-09 02:27:06 +0530

    UserGrid for Team Roster

--- REACT_JS/01_React_App/src/05_User_Role_Filter/App.jsx
+++ REACT_JS/01_React_App/src/05_User_Role_Filter/App.jsx
@@ -1,15 +1,13 @@
-import UserCard from "./Components/UserCard";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import UserGrid from "./Components/UserGrid";
 
 function App() {
     return (
         <div className="min-h-screen bg-slate-950 text-slate-100 ">
             <Header totalProduct={8} />
             <Navbar />
-            <div className="px-10">
-                <UserCard />
-            </div>
+            <UserGrid />
         </div>
     );
 }
---
+++ REACT_JS/01_React_App/src/05_User_Role_Filter/Components/UserGrid.jsx
@@ -0,0 +1,10 @@
+import UserCard from "./UserCard";
+
+function UserGrid() {
+    return (
+        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center p-10">
+        </main>
+    );
+}
+
+export default UserGrid;
