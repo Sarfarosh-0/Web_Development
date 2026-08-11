228c1d7b0625c919abbca631679f0b8da95feb2f
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-11 23:22:59 +0530

    Inital Setup For Expandable FAQ

---
+++ REACT_JS/02_React_App/src/01_Expandable_FAQ/01_Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import FAQ from "./FAQ.jsx";
+
+
+createRoot(document.getElementById("root")).render(
+    <strictMode>
+        <FAQ />
+    </strictMode>
+
+
+)
