88adf0e64ac5a34387d52c0480bb772993ad3bae
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-12 22:01:34 +0530

    Added arrown Emoji

--- REACT_JS/02_React_App/src/01_Expandable_FAQ/01_Main.jsx
+++ REACT_JS/02_React_App/src/01_Expandable_FAQ/01_Main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import FAQ from "./FAQ.jsx";
 
+import '../../src/index.css'
+
 
 createRoot(document.getElementById("root")).render(
     <strictMode>
-        <FAQ question={"what is REACT ?"} answer={"REACT is a JAVASCRIPT Libray"}/>
+        <div  className="flex flex-col justify-center items-center m-10">
+            <FAQ question={"What is REACT ?"} answer={"REACT is a JAVASCRIPT Libray"} />
+        </div>
     </strictMode>
 
 
--- REACT_JS/02_React_App/src/01_Expandable_FAQ/FAQ.jsx
+++ REACT_JS/02_React_App/src/01_Expandable_FAQ/FAQ.jsx
@@ -9,12 +9,19 @@ function FAQ({ question, answer }) {
                 onClick={() => {
                     setOpenClose(!openClose);
                 }}
+                className="cursor-pointer text-3xl font-bold border-slate-400 border bg-gray-400 p-5 rounded-lg w-1/2 flex justify-between items-center"
             >
-                {question}
+                <span>{question}</span>
+                <span
+                    className={`inline-block transition-transform duration-300 ${openClose ? 'rotate-180' : 'rotate-0'}`}
+                >
+                    ⬇️
+                </span>
+
             </h1>
 
             {openClose && (
-                <p>{answer}</p>
+                <p className="mt-2 p-3 text-xl w-1/2">{answer}</p>
             )}
         </>
     );
