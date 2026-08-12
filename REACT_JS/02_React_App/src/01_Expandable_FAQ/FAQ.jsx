e4f73c7cb3e5654d471046496ba7da86f69b5d0e
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-12 22:09:08 +0530

    Chnaged COlORS

--- REACT_JS/02_React_App/src/01_Expandable_FAQ/FAQ.jsx
+++ REACT_JS/02_React_App/src/01_Expandable_FAQ/FAQ.jsx
@@ -7,7 +7,7 @@ function FAQ({ question, answer }) {
         <div className="w-1/2">
             <h1
                 onClick={() => setOpenClose(!openClose)}
-                className={`cursor-pointer text-2xl font-bold border-indigo-200 border bg-indigo-900 text-white p-5 flex justify-between items-center transition-all duration-300 ${
+                className={`cursor-pointer text-2xl font-bold border-slate-800 border bg-slate-900 text-slate-100 p-5 flex justify-between items-center transition-all duration-300 ${
                     openClose ? "rounded-t-xl" : "rounded-xl shadow-md"
                 }`}
             >
@@ -18,7 +18,7 @@ function FAQ({ question, answer }) {
                     viewBox="0 0 24 24" 
                     strokeWidth={2.5} 
                     stroke="currentColor" 
-                    className={`w-6 h-6 text-emerald-400 transition-transform duration-300 ${
+                    className={`w-6 h-6 text-cyan-400 transition-transform duration-300 ${
                         openClose ? "rotate-180" : "rotate-0"
                     }`}
                 >
@@ -27,13 +27,13 @@ function FAQ({ question, answer }) {
             </h1>
 
             <div
-                className={`transition-all duration-500 ease-in-out overflow-hidden border-indigo-200 bg-emerald-50 rounded-b-xl ${
+                className={`transition-all duration-500 ease-in-out overflow-hidden border-slate-200 bg-slate-50 rounded-b-xl ${
                     openClose 
                         ? "max-h-96 opacity-100 border-x border-b shadow-inner" 
                         : "max-h-0 opacity-0 border-x-0 border-b-0"
                 }`}
             >
-                <p className="text-xl text-indigo-950 font-medium p-5">
+                <p className="text-xl text-slate-800 font-medium p-5">
                     {answer}
                 </p>
             </div>
