bb54145924e0c55948dad2215f4c136cc0e8f8b2
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-13 23:12:14 +0530

    Fixed Question

--- REACT_JS/02_React_App/src/01_Expandable_FAQ/01_Main.jsx
+++ REACT_JS/02_React_App/src/01_Expandable_FAQ/01_Main.jsx
@@ -8,7 +8,7 @@ import '../../src/index.css'
 createRoot(document.getElementById("root")).render(
     <strictMode>
         <div  className="flex flex-col justify-center items-center gap-2 m-10">
-            <FAQ question={"What is REACT ?"} answer={"REACT is a JAVASCRIPT Libray"} />
+            <FAQ question={"What is HTML ?"} answer={"HTML stands for HyperText Markup Language, and it is the standard markup language used to create and structure content on the World Wide Web. It acts as the skeleton or foundational blueprint of any website, telling web browsers (like Chrome, Safari, or Firefox) how to display text, images, links, and multimedia."} />   
         </div>
     </strictMode>
 
