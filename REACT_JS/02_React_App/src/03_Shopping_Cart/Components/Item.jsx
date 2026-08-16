dd132bfe29dac8792dfa1318ca799f96c7551482
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-16 18:47:27 +0530

    Final ItemContainer

--- REACT_JS/02_React_App/src/03_Shopping_Cart/Components/Item.jsx
+++ REACT_JS/02_React_App/src/03_Shopping_Cart/Components/Item.jsx
@@ -1,10 +1,10 @@
 function Item() {
     return (
-        <div className="itemCard bg-white border-b border-slate-300 py-2 px-4 w-full grid grid-cols-8 gap-5 items-center">
+        <div className="itemCard hover:bg-gray-100 bg-white border-b border-slate-300 rounded-xl py-2 px-4 w-full grid grid-cols-8 gap-4 items-center">
             {/* Product Image */}
             <img
                 src="https://i.pinimg.com/736x/88/40/c3/8840c3a4db6fc894291dd9b578450244.jpg"
-                alt="Hoodie"
+                alt="Hoodie" draggable="false"
                 className="w-20 h-20 object-cover rounded-lg border border-gray-200 col-span-1"
             />
 
@@ -18,7 +18,7 @@ function Item() {
             <h1 className="Price font-semibold text-gray-600 col-span-1 text-base">$39.99</h1>
 
             {/* Product Quantity */}
-            <div className="col-span-2 quantity px-2 py-1 flex items-center gap-3 bg-gray-100/80 rounded-lg border border-gray-200 text-gray-800 font-medium shadow-inner">
+            <div className="col-span-2 w-fit quantity px-2 py-1 flex items-center gap-3 bg-gray-100/80 rounded-lg border border-gray-200 text-gray-800 font-medium shadow-inner">
 
                 {/* Minus SVG Button */}
                 <button
@@ -49,7 +49,7 @@ function Item() {
             {/* Delete Button */}
             <button
                 aria-label="Remove item"
-                className="removeItem col-span-1 p-2 rounded-lg text-red-500 hover:bg-red-50 hover:text-red-600 transition-colors active:scale-90">
+                className="removeItem w-fit col-span-1 p-2 rounded-lg text-red-500 hover:bg-red-50 hover:text-red-600 transition-colors active:scale-90">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5"
                 ><path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
--- REACT_JS/02_React_App/src/03_Shopping_Cart/Components/ItemContainer.jsx
+++ REACT_JS/02_React_App/src/03_Shopping_Cart/Components/ItemContainer.jsx
@@ -2,21 +2,25 @@ import Item from "./Item";
 
 function ItemContainer() {
     return (
-        <section className="m-5 p-2 bg-white border border-gray-200 rounded-xl w-full max-w-3xl shadow-sm flex flex-col">
+        <section className="m-5 p-2 bg-white border border-gray-200 rounded-xl w-full max-w-3xl shadow-sm flex flex-col items-center">
 
             {/* Table Headers */}
-            <div className="border-gray-200 text-sm font-bold text-black uppercase bg-gray-50 border-b py-2 px-4 w-full grid grid-cols-8 gap-5 items-center">
-                
+            <div className="border-gray-200 text-sm font-bold text-black uppercase bg-gray-50 rounded-2xl border-b py-5 px-4 w-full grid grid-cols-8 gap-4 items-center">
+
                 <span className="col-span-1">Item</span>
                 <span className="col-span-2"></span>
-                <span className="col-span-1 text-center">Price</span>
-                <span className="col-span-2 text-center">Quantity</span>
-                <span className="col-span-1 text-center">Total</span>
+                <span className="col-span-1">Price</span>
+                <span className="col-span-2">Quantity</span>
+                <span className="col-span-1">Total</span>
                 <span className="col-span-1 text-right"></span>
             </div>
 
             {/* Scrollable Item List */}
-            <div className="overflow-auto scrollbar-thin max-h-[550px] flex flex-col gap-3 pr-1">
+            <div className="overflow-auto scrollbar-thin max-h-[550px] flex flex-col ">
+                <Item />
+                <Item />
+                <Item />
+                <Item />
                 <Item />
                 <Item />
                 <Item />
@@ -29,18 +33,11 @@ function ItemContainer() {
 
             {/* Footer Action */}
             <div className="pt-2">
-                <button className="px-4 py-2.5 border border-violet-500 text-violet-600 bg-violet-50 hover:bg-violet-100 transition-colors rounded-lg font-semibold flex items-center gap-2 text-sm">
+                <button className="px-4 py-2.5 border border-violet-500 text-violet-600 bg-violet-50 hover:bg-violet-100  rounded-lg font-semibold flex items-center gap-2 text-sm active:scale-90 transition-all ease-in-out duration-300">
                     {/* Left Arrow SVG */}
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="w-4 h-4"
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-4 h-4"> <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                     </svg>
+
                     Continue Shopping
                 </button>
             </div>
