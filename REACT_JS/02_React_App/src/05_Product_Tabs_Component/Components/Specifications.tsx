c9a7175dca792b818000851a10021915e8cde5ec
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-08-21 00:11:47 +0530

    Scorllbar for Specifications

--- REACT_JS/02_React_App/src/05_Product_Tabs_Component/Components/Specifications.tsx
+++ REACT_JS/02_React_App/src/05_Product_Tabs_Component/Components/Specifications.tsx
@@ -7,16 +7,17 @@ function Specifications() {
         <section id="specifications" className="w-5xl p-10 bg-white border-b border-slate-400 rounded-b-2xl shadow flex flex-col gap-2">
             <h1 className="font-bold text-2xl">Specificatsions</h1>
             <p className="text-base text-slate-500">Detailed information about the products's features and techniacl specifications</p>
-            <div className="flex flex-col items-center justify-center py-2 max-h-2xl overflow-y-auto scrollbar-thin">
-                {
-                    specifications.map((detail) => (
+
+            <div className="max-h-80 overflow-y-auto py-2 pr-2 [scrollbar-color:#cbd5e1_transparent] scrollbar-thin [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-slate-300 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-1.5">
+                <div className="flex flex-col">
+                    {specifications.map((detail) => (
                         <ProductSpecifications
                             key={detail.id}
                             label={detail.label}
                             value={detail.value}
                         />
-                    ))
-                }
+                    ))}
+                </div>
             </div>
         </section>
     )
